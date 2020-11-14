33e2e2c9bfab5d51b8fc7ba51c8740d6387ff350
ponte1010 <t.miwa.528@nitech.jp>
2020-11-14 14:40:40 +0900

    Add   Constraints

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tapAction(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
