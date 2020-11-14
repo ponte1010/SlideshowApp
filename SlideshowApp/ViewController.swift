d3271b38348f86a19ed1729b8707d08fa6a1f91a
ponte1010 <t.miwa.528@nitech.jp>
2020-11-14 15:26:02 +0900

    unwind segue

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,9 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func tapAction(_ sender: Any) {
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
